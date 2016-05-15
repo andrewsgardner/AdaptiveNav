e8b69d97b1b2cab1e7b2c4ca8c646ad6506bd203
Andrew S. Gardner <asg@andrewsgardner.com>
2016-05-15 17:35:09 -0400

    Added path info for expanded CSS and JS.

---
+++ resources/config.php
@@ -0,0 +1,25 @@
+<?php
+$config = array(
+    "info" => array(
+        "cacheVer" => "?v=2.0"
+        ),
+    "paths" => array(
+        "css" => array(
+            "expanded" => "/css/adaptivenav.css",
+            "min" => "/css/adaptivenav.min.css"
+        ),
+        "scripts" => array(
+            "expanded" => "/js/adaptivenav.js",
+            "min" => "/js/adaptivenav.min.js",
+            "html5shiv" => "https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"
+        )
+    ),
+    "jQuery" => array(
+        "cdn" => "https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js",
+        "local" => "/js/local/jquery-1.12.2.min.js"
+    )
+);
+
+defined("TEMPLATES_PATH")
+	or define("TEMPLATES_PATH", realpath(dirname(__FILE__) . "/templates"));
+?>
