6ebab636fce9ff14d1a1bed961bec766913c8458
Andrew S. Gardner <asg@andrewsgardner.com>
2016-05-15 15:48:19 -0400

    Added public_html dir.

---
+++ public_html/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title>AdaptiveNav v2.0 - Responsive Navigation Menu</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- AdaptiveNav stylesheet -->
+	<link rel="stylesheet" type="text/css" href="/css/adaptivenav.min.css"/>
+</head>
+<body>
+    <!-- BEGIN AdaptiveNav -->
+    <!-- END AdaptiveNav -->
+</body>
+</html>
