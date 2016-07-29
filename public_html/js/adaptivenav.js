0b93ab912101bfacc610d5eb4212cfb1c1c0186d
Andrew S. Gardner <asg@andrewsgardner.com>
2016-07-29 14:07:08 -0400

    Update animation styles

--- public_html/js/adaptivenav.js
+++ public_html/js/adaptivenav.js
@@ -2,24 +2,35 @@ $(document).ready(function() {
     
     $(window).scroll(function() {
        if ($(this).scrollTop() > 1) {
+           
            /* add header animation */
-           $('header').addClass('scroll-text scroll-animation');
+           $('header').addClass('scroll-nav scroll-animation');
+           
            /* add hamburger icon animation */
            $('.hamburger-icon').addClass('scroll-icon');
+           
        } else {
+           
            /* remove header animation */
-           $('header').removeClass('scroll-text').addClass('scroll-animation');
+           $('header').removeClass('scroll-nav').addClass('scroll-animation');
+           
            /* remove hamburger icon animation */
            $('.hamburger-icon').removeClass('scroll-icon').addClass('scroll-animation');
+           
        }
     });
     
 });;
 $(document).ready(function() {
     
-    /* render hamburger icon */
-    $('.hamburger-icon').click(function() {
+    $('.hamburgerTrigger').click(function() {
+        
+        /* render hamburger icon */
         $('nav.primary-menu ul').slideToggle();
+        
+        /* center logo on click */
+        $('.logo').toggleClass('pushLogo');
+        
     });
     
 });;
@@ -28,8 +39,10 @@ $(document).ready(function() {
     function onReadyResize() {
         
         if ($(window).width() <= 767) {
+            
             /* remove mobile header styles when switching back to desktop mode */
             $('nav.primary-menu ul').removeAttr('style');
+            
         }
         
     }
