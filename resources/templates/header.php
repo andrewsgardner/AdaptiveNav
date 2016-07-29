d73f1d8730244ab3fe00eda2209ecc557e8df7ca
Andrew S. Gardner <asg@andrewsgardner.com>
2016-07-29 09:55:41 -0400

    Update header markup

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -1,32 +1,35 @@
-<?php
-include(dirname(__FILE__) . "/../config.php");
-
+<?php 
 /* BEGIN header */
-echo "\t\t<header>\n";
+echo "<header>\n";
+
+/* BEGIN navbar */
+echo "<div class=\"nav-container\">\n";
 
 /* BEGIN logo */
-echo "\t\t\t<div class=\"logo\">\n";
-echo "\t\t\t\t"?><a href="#"><img src="<?php echo $config['paths']['img']['layout'] . "/logo.png"; ?>" alt="AdaptiveNav v2.0"></a><?php 
-echo "\n\t\t\t</div>\n";
+echo "<div class=\"logo\">\n";
+echo "<a href=\"" . "#" . "\"><img src=\"" . $config['paths']['img']['layout'] . "/logo.png" . "\" alt=\"AdaptiveNav v2.0" . $config['info']['author'] . "\"></a>\n";
+echo "</div>\n";
 /* END logo */
 
 /* BEGIN primary nav */
-echo "\t\t\t<nav class=\"primary-menu\">\n";
-
+echo "<nav class=\"primary-menu\">\n";
 /* BEGIN hamburger icon */
-echo "\t\t\t<div class=\"hamburger-icon\"></div>\n";
+echo "<div class=\"hamburgerTrigger\">\n";
+echo "<span class=\"hamburger-icon\"></span>\n";
+echo "</div>\n";
 /* END hamburger icon */
-
-echo "\t\t\t<ul>\n"; ?>
-<?php echo "\t\t\t\t"; ?><li><a href="#"<?php if($currentPage == 'link_1') echo ' class="active"'; ?>>Link 1</a></li><?php echo "\n"; ?>
-<?php echo "\t\t\t\t"; ?><li><a href="#"<?php if($currentPage == 'Link_2') echo ' class="active"'; ?>>Link 2</a></li><?php echo "\n"; ?>
-<?php echo "\t\t\t\t"; ?><li><a href="#"<?php if($currentPage == 'link_3') echo ' class="active"'; ?>>Link 3</a></li><?php echo "\n"; ?>
-<?php echo "\t\t\t\t"; ?><li><a href="#"<?php if($currentPage == 'link_4') echo ' class="active"'; ?>>Link 4</a></li><?php echo "\n"; ?>
-<?php echo "\t\t\t\t"; ?><li><a href="#"<?php if($currentPage == 'link_5') echo ' class="active"'; ?>>Link 5</a></li><?php echo "\n"; ?>
-<?php echo "\t\t\t</ul>\n";
-echo "\t\t\t</nav>\n";
+echo "<ul>\n";
+echo "<li><a href=\"" . $headerRoutes['demoDir1'] . "\""?><?php if($currentPage == 'link_1') echo ' class="active"'; ?><?php echo ">Link 1</a></li>\n";
+echo "<li><a href=\"" . $headerRoutes['demoDir2'] . "\""?><?php if($currentPage == 'link_2') echo ' class="active"'; ?><?php echo ">Link 2</a></li>\n";
+echo "<li><a href=\"" . $headerRoutes['demoDir3'] . "\""?><?php if($currentPage == 'link_3') echo ' class="active"'; ?><?php echo ">Link 3</a></li>\n";
+echo "<li><a href=\"" . $headerRoutes['demoDir4'] . "\""?><?php if($currentPage == 'link_4') echo ' class="active"'; ?><?php echo ">Link 4</a></li>\n";
+echo "</ul>\n";
+echo "</nav>\n";
 /* END primary nav */
 
-echo "\t\t</header>\n";
+echo "</div>\n";
+/* END navbar */
+
+echo "</header>\n";
 /* End header */
 ?>
