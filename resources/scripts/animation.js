25a2f50c41c4808a9000fbf0d1915f662da4ae3b
Andrew S. Gardner <asg@andrewsgardner.com>
2016-07-29 14:00:58 -0400

    Changed .scroll-text to .scroll-nav

--- resources/scripts/animation.js
+++ resources/scripts/animation.js
@@ -2,15 +2,21 @@ $(document).ready(function() {
     
     $(window).scroll(function() {
        if ($(this).scrollTop() > 1) {
+           
            /* add header animation */
-           $('header').addClass('scroll-text scroll-animation');
+           $('header').addClass('scroll-nav scroll-animation');
+           
            /* add hamburger icon animation */
            $('.hamburger-icon').addClass('scroll-icon');
+           
        } else {
+           
            /* remove header animation */
-           $('header').removeClass('scroll-text').addClass('scroll-animation');
+           $('header').removeClass('scroll-nav').addClass('scroll-animation');
+           
            /* remove hamburger icon animation */
            $('.hamburger-icon').removeClass('scroll-icon').addClass('scroll-animation');
+           
        }
     });
     
