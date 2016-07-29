adda5d7608f4db98904dde39f2a06e1b2818c371
Andrew S. Gardner <asg@andrewsgardner.com>
2016-07-29 10:12:52 -0400

    Add example page for link 2

---
+++ public_html/link-2/index.php
@@ -0,0 +1,28 @@
+<?php 
+$currentPage = "link_2";
+require_once("config.php"); 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>AdaptiveNav v2.0 - Responsive Navigation Menu</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php echo "\t<!--[if lt IE 9]>\n\t\t<script src=\"" . $config['paths']['scripts']['html5shiv'] . "\"></script>\n\t<![endif]-->\n"; ?>
+    
+    <!-- AdaptiveNav stylesheet -->
+    <link rel="stylesheet" type="text/css" href="<?php echo $config['paths']['css']['expanded'] . $config['info']['cacheVer']; ?>">
+</head>
+<body>
+    <!-- BEGIN AdaptiveNav -->
+    
+    <?php require_once(TEMPLATES_PATH . "/header.php"); ?>
+    
+    <h1>Link 2</h1>
+    
+    <!-- END AdaptiveNav -->
+    
+    <?php require_once(TEMPLATES_PATH . "/loadScripts.php"); ?>
+</body>
+</html>
