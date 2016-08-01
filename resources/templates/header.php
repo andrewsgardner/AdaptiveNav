5c3e0a2ec0323f2e43ad3ee7af3058d4b0c07be3
Andrew S. Gardner <asg@andrewsgardner.com>
2016-07-31 23:42:52 -0400

    Update markup formatting

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -3,27 +3,27 @@
 echo "<header>\n";
 
 /* BEGIN logo */
-echo "<div class=\"logo\">\n";
-echo "<a href=\"" . $headerRoutes['demoDir1'] . "\"><img src=\"" . $config['paths']['img']['layout'] . "/logo.svg" . "\" alt=\"AdaptiveNav v2.0" . "\"></a>\n";
-echo "</div>\n";
+echo "\t\t<div class=\"logo\">\n";
+echo "\t\t\t<a href=\"" . $headerRoutes['demoDir1'] . "\"><img src=\"" . $config['paths']['img']['layout'] . "/logo.svg" . "\" alt=\"AdaptiveNav v2.0" . "\"></a>\n";
+echo "\t\t</div>\n";
 /* END logo */
 
 /* BEGIN primary nav */
-echo "<nav class=\"primary-menu\">\n";
+echo "\t\t<nav class=\"primary-menu\">\n";
 /* BEGIN hamburger icon */
-echo "<div class=\"hamburgerTrigger\">\n";
-echo "<span class=\"hamburger-icon\"></span>\n";
-echo "</div>\n";
+echo "\t\t\t<div class=\"hamburgerTrigger\">\n";
+echo "\t\t\t\t<span class=\"hamburger-icon\"></span>\n";
+echo "\t\t\t</div>\n";
 /* END hamburger icon */
-echo "<ul>\n";
-echo "<li><a href=\"" . $headerRoutes['demoDir1'] . "\""?><?php if($currentPage == 'link_1') echo ' class="active"'; ?><?php echo ">Link 1</a></li>\n";
-echo "<li><a href=\"" . $headerRoutes['demoDir2'] . "\""?><?php if($currentPage == 'link_2') echo ' class="active"'; ?><?php echo ">Link 2</a></li>\n";
-echo "<li><a href=\"" . $headerRoutes['demoDir3'] . "\""?><?php if($currentPage == 'link_3') echo ' class="active"'; ?><?php echo ">Link 3</a></li>\n";
-echo "<li><a href=\"" . $headerRoutes['demoDir4'] . "\""?><?php if($currentPage == 'link_4') echo ' class="active"'; ?><?php echo ">Link 4</a></li>\n";
-echo "</ul>\n";
-echo "</nav>\n";
+echo "\t\t\t<ul>\n";
+echo "\t\t\t\t<li><a href=\"" . $headerRoutes['demoDir1'] . "\""?><?php if($currentPage == 'link_1') echo ' class="active"'; ?><?php echo ">Link 1</a></li>\n";
+echo "\t\t\t\t<li><a href=\"" . $headerRoutes['demoDir2'] . "\""?><?php if($currentPage == 'link_2') echo ' class="active"'; ?><?php echo ">Link 2</a></li>\n";
+echo "\t\t\t\t<li><a href=\"" . $headerRoutes['demoDir3'] . "\""?><?php if($currentPage == 'link_3') echo ' class="active"'; ?><?php echo ">Link 3</a></li>\n";
+echo "\t\t\t\t<li><a href=\"" . $headerRoutes['demoDir4'] . "\""?><?php if($currentPage == 'link_4') echo ' class="active"'; ?><?php echo ">Link 4</a></li>\n";
+echo "\t\t\t</ul>\n";
+echo "\t\t</nav>\n";
 /* END primary nav */
 
-echo "</header>\n";
+echo "\t</header>\n";
 /* End header */
 ?>
--- resources/templates/loadScripts.php
+++ resources/templates/loadScripts.php
@@ -1,5 +1,5 @@
 <?php 
 echo "<script src=\"" . $config['jQuery']['cdn'] . "\"></script>\n";
-echo "<script>window.jQuery || document.write('<script src=\"" . $config['jQuery']['local'] . "\"><\/script>')</script>\n";
-echo "<script src=\"" . $config['paths']['scripts']['expanded'] . $config['info']['cacheVer'] . "\"></script>\n";
+echo "\t<script>window.jQuery || document.write('<script src=\"" . $config['jQuery']['local'] . "\"><\/script>')</script>\n";
+echo "\t<script src=\"" . $config['paths']['scripts']['expanded'] . $config['info']['cacheVer'] . "\"></script>\n";
 ?>
