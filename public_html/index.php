71fbf3e992eef7be967cface87d862f5073eb1a0
Andrew S. Gardner <asg@andrewsgardner.com>
2016-07-31 23:32:33 -0400

    Update markup formatting

--- public_html/index.php
+++ public_html/index.php
@@ -9,7 +9,7 @@ require_once("config.php");
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>AdaptiveNav v2.0 - Responsive Navigation Menu: Link 1</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php echo "\t<!--[if lt IE 9]>\n\t\t<script src=\"" . $config['paths']['scripts']['html5shiv'] . "\"></script>\n\t<![endif]-->\n"; ?>
+    <?php echo "<!--[if lt IE 9]>\n\t\t<script src=\"" . $config['paths']['scripts']['html5shiv'] . "\"></script>\n\t<![endif]-->\n"; ?>
     
     <!-- AdaptiveNav stylesheet -->
     <link rel="stylesheet" type="text/css" href="<?php echo $config['paths']['css']['expanded'] . $config['info']['cacheVer']; ?>">
